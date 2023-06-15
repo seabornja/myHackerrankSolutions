cb3e8fd70287e7707d92a0dce1a8b079ffefa22d
seabornja <johnseaborn$>
2023-06-15 13:48:10 -0400

    final code

--- 013whats-your-name/code/whats-your-name.py
+++ 013whats-your-name/code/whats-your-name.py
@@ -11,7 +11,7 @@ def print_full_name(first, last):
     line_1 = "Hello "
     outputString = line_1 + first
     outputString = outputString + " " + last
-    outputString = outputString + "! You have just delved into python."
+    outputString = outputString + "! You just delved into python."
     print(outputString)
 
 if __name__ == '__main__':
