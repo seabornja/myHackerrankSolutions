40bf966ac462ff31255650298246c74917a83dac
seabornja <johnseaborn$>
2023-06-15 15:38:59 -0400

    second commit

--- 010list-comprehensions/code/list-comprehensions.py
+++ 010list-comprehensions/code/list-comprehensions.py
@@ -3,3 +3,7 @@ if __name__ == '__main__':
     y = int(input())
     z = int(input())
     n = int(input())
+    cuboid_x = [x for x in range(x) if x != n]
+    cuboid_y = [y for x in range(y) if y != n]
+    cuboid_z = [z for z in range(z) if z != n]
+    print(cuboid_x, cuboid_y, cuboid_z)
